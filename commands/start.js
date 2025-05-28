3ec6542cbe3bfe309732d6e9c61a89df49239941
Yannbot1 <yannbot570@gmail.com>
2025-05-29 00:31:34 +0100

    Update start.js

--- commands/start.js
+++ commands/start.js
@@ -2,8 +2,8 @@ module.exports = (bot) => {
   bot.command('start', (ctx) => {
     const userName = ctx.from.first_name; 
 
-    const welcomeMessage = `👋 Bonjour ${userName} \n\nBienvenue chère utilisateur ou utilisatrice moi c'est Dan jersey bot \n\n- /help consultez la liste de mes commandes \n\n veuillez contacter @Dan_jersey si le bot à un problème 🤗`;
-
+    const welcomeMessage = `👋 Bonjour ${userName} \n\nBienvenue chère utilisateur ou utilisatrice moi c'est Yann bot \n\n- /help consultez la liste de mes commandes \n\n veuillez contacter si le bot à un problème 🤗`;
+@jachiiuser
     ctx.reply(welcomeMessage);
   });
 };
